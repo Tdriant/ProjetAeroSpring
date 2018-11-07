3a3bbe264156f1a96d61156db3313b5f05eea961
Unknown <andrieu15@hotmail.com>
2018-11-07 11:53:24 +0100

    CaVol updated

--- src/test/java/TestsCaVol.java
+++ src/test/java/TestsCaVol.java
@@ -1,8 +1,10 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -12,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 import model.CompagnieAerienne;
 import model.CompagnieAerienneVol;
 import model.CompagnieAerienneVolKey;
@@ -21,93 +22,59 @@ import repositories.CaVolRepository;
 import repositories.CompagnieAerienneRepository;
 import repositories.VolRepository;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/applicationcontext.xml" })
 public class TestsCaVol {
 
-	@Autowired 
-	private static CaVolRepository CaVolRepo;
 	@Autowired
-	private static VolRepository volRepo;
+	private   CaVolRepository CaVolRepo;
+
 	@Autowired
-	private static CompagnieAerienneRepository caRepo;
+	private  CompagnieAerienneRepository caRepo;
 
-
+	@Autowired
+	private VolRepository volRepo;
+	
+	
 
 	@Test
-	public static void initDaoCAVol() {
+	public  void initDaoCAVol() {
 
 		SimpleDateFormat date = new SimpleDateFormat("DD/MM/YYYY");
 		SimpleDateFormat heure = new SimpleDateFormat("HH-mm-ss");
 		try {
-			Vol v1 = new Vol(date.parse("26/10/2018"), date.parse("27/10/2018"), heure.parse("11-50-00"), heure.parse("06-45-00"));
-			Vol v2 = new Vol(date.parse("15/05/2009"), date.parse("15/05/2009"), heure.parse("06-05-00"), heure.parse("12-30-00"));
+			Vol v1 = new Vol(date.parse("26/10/2018"), date.parse("27/10/2018"), heure.parse("11-50-00"),
+					heure.parse("06-45-00"));
+			Vol v2 = new Vol(date.parse("15/05/2009"), date.parse("15/05/2009"), heure.parse("06-05-00"),
+					heure.parse("12-30-00"));
 			CompagnieAerienne ca1 = new CompagnieAerienne("air france");
 			CompagnieAerienne ca2 = new CompagnieAerienne("delta");
-			assertNull(v1.getId());
-			assertNull(v2.getId());
-			assertNull(ca1.getId());
-			assertNull(ca2.getId());
 			volRepo.save(v1);
 			volRepo.save(v2);
 			caRepo.save(ca1);
 			caRepo.save(ca2);
-			CaVolRepo cav1 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v1));
-			CaVolRepo cav2 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v1));
-			CaVolRepo cav3 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v2));;
+			CompagnieAerienneVol cav1 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v1));
+			CompagnieAerienneVol cav2 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v1));
+			CompagnieAerienneVol cav3 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v2));
+			CaVolRepo.save(cav1);
+			CaVolRepo.save(cav2);
+			CaVolRepo.save(cav3);
 
 			assertNotNull(v1.getId());
 			assertNotNull(v2.getId());
 			assertNotNull(ca1.getId());
 			assertNotNull(ca2.getId());
+			assertNotNull(cav1.getKey());
+			assertNotNull(cav2.getKey());
+			assertNotNull(cav3.getKey());
+			assertNotNull(CaVolRepo.findById(cav1.getKey()));
+			System.out.println(CaVolRepo.findAll());
+			CaVolRepo.deleteById(cav1.getKey());
+			Optional<CompagnieAerienneVol> optt = CaVolRepo.findById(cav1.getKey());
+			assertFalse(optt.isPresent());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
-
-
-	@Test
-	public void insert() {
-		CompagnieAerienneVol cav1 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v1));
-		assertNotNull(cav1.getKey());
-		assertNotNull(cav1.getKey().getVol());
-		assertNotNull(cav1.getKey().getCompagnieAerienne());
-		daoCAVol.create(cav1);
-	}
-
-	@Test
-	public void findByKey() {
-		CompagnieAerienneVol cav2 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca2, v2));
-		daoCAVol.create(cav2);
-		assertNotNull(daoCAVol.findByKey(new CompagnieAerienneVolKey(cav2.getKey().getCompagnieAerienne(), cav2.getKey().getVol())));
-	}
-
-
-	@Test
-	public void findAll() {
-		assertNotNull(daoCAVol.findAll());
-	}
-
-	@Test
-	public void delete() {
-		CompagnieAerienneVol cav3 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v2));;
-		daoCAVol.create(cav3);
-		assertNotNull(daoCAVol.findByKey(new CompagnieAerienneVolKey(cav3.getKey().getCompagnieAerienne(), cav3.getKey().getVol())));
-		cav3 = daoCAVol.findByKey(cav3.getKey());
-		daoCAVol.delete(cav3);
-		assertNull(daoCAVol.findByKey(new CompagnieAerienneVolKey(cav3.getKey().getCompagnieAerienne(), cav3.getKey().getVol())));
-	}
-
-	@Test
-	public void deleteByKey() {
-	CompagnieAerienneVol cav3 = new CompagnieAerienneVol(new CompagnieAerienneVolKey(ca1, v2));;
-	daoCAVol.create(cav3);
-	assertNotNull(daoCAVol.findByKey(new CompagnieAerienneVolKey(cav3.getKey().getCompagnieAerienne(), cav3.getKey().getVol())));
-	cav3 = daoCAVol.findByKey(cav3.getKey());
-	daoCAVol.deleteByKey(cav3.getKey());
-	assertNull(daoCAVol.findByKey(new CompagnieAerienneVolKey(cav3.getKey().getCompagnieAerienne(), cav3.getKey().getVol())));
-	}
+	
 }
