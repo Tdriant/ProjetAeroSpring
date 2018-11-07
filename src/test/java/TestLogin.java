804198c3d157c117383ffab14fa17608107a9e8c
Soubika <soubika.bisoo@gmail.com>
2018-11-07 11:39:25 +0100

    test login

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -1,19 +1,16 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Optional;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import model.Client;
-import model.ClientMoral;
 import model.Login;
-import model.TitreMoral;
 import repositories.LoginRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -47,45 +44,44 @@ public class TestLogin {
 		}
 	}
 
-//	@Test
+	@Test
 	public void update() {
 		Login login = new Login("tutu", "huhuhu", true);
 		loginRepository.save(login);
 		assertNotNull(loginRepository.findById(login.getId()));
-//		Optional<Login> log = loginRepository.findById(login.getId());
-//		if (log.isPresent()) {
-//			assertNotNull(log.get());
-//		}
-//		login.setLogin("coco");
-//		loginRepository.save(login);
-//		log = loginRepository.findById(login.getId());
-//		if (log.isPresent()) {
-//			assertEquals("coco", log.get().getLogin());
-//		}
+		Optional<Login> log = loginRepository.findById(login.getId());
+		if (log.isPresent()) {
+			assertNotNull(log.get());
+		}
+		login.setLogin("coco");
+		loginRepository.save(login);
+		log = loginRepository.findById(login.getId());
+		if (log.isPresent()) {
+			assertEquals("coco", log.get().getLogin());
+		}
 
 	}
 
-//	@Test
-//	public void FindAll() {
-//		assertNotNull(daoLogin.findAll());
-//	}
-//
-//	@Test
-//	public void delete() {
-//		Login login = new Login("toto", "tata", true);
-//		daoLogin.create(login);
-//		daoLogin.delete(login);
-//		assertNull(daoLogin.findByKey(login.getId()));
-//
-//	}
-//
-//	@Test
-//	public void deleteByKey() {
-//		Login login = new Login("toto", "tata", true);
-//		daoLogin.create(login);
-//		daoLogin.deleteByKey(login.getId());
-//		assertNull(daoLogin.findByKey(login.getId()));
-//
-//	}
+	@Test
+	public void FindAll() {
+		assertNotNull(loginRepository.findAll());
+	}
+
+	@Test
+	public void delete() {
+		Login login = new Login("toto", "tata", true);
+		loginRepository.save(login);
+		loginRepository.delete(login);
+		assertFalse(loginRepository.findById(login.getId()).isPresent());
+
+	}
+
+	@Test
+	public void deleteByKey() {
+		Login login = new Login("toto", "tata", true);
+		loginRepository.save(login);
+		loginRepository.deleteById(login.getId());
+		assertFalse(loginRepository.findById(login.getId()).isPresent());
+	}
 
 }
