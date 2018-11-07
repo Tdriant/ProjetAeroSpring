756e6f4b9efb077c8ed9e3ef77ca0645e8dc8e15
Unknown <andrieu15@hotmail.com>
2018-11-07 11:11:26 +0100

    TestVol updated

--- src/test/java/TestVol.java
+++ src/test/java/TestVol.java
@@ -2,17 +2,18 @@ import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
+import java.util.Optional;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import model.CompagnieAerienne;
 import model.Vol;
 import repositories.VolRepository;
 
-import static org.junit.Assert.*;
+
 
 
 import org.junit.Test;
@@ -26,22 +27,32 @@ public class TestVol {
 	@Test
 	public void tryVol() {
 	SimpleDateFormat date = new SimpleDateFormat("DD/MM/YYYY");
-	Vol bouwa = new Vol();
-	Vol bouwi = new Vol();
-	Vol Baboum = new Vol();
-	
-	
+
 	try
 	{
+		Vol bouwa = new Vol();
+	Vol bouwi = new Vol(date.parse("12/12/2018"), date.parse("12/12/2019"));
+	Vol Baboum = new Vol(date.parse("07/11/2018"), date.parse("08/11/2018"));
+	
 		bouwa.setDateDepart(date.parse("12/12/1998"));
 		bouwa.setDateArrivee(date.parse("12/12/1998"));
+		volRepo.save(bouwa);
+		volRepo.save(bouwi);
+		volRepo.save(Baboum);
+		bouwa.setDateArrivee(date.parse("14/12/1998"));
+		volRepo.save(bouwa);
+		volRepo.deleteById(bouwa.getId());
+		assertNotNull(volRepo.findAll());
+		assertNotNull(volRepo.findById(Baboum.getId()));
+
+		Optional<Vol> optt = volRepo.findById(bouwa.getId());
+		assertFalse(optt.isPresent());
 	}catch(
 	ParseException e)
 	{
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	volRepo.save(bouwa);
 	assertNotNull(volRepo);
 }
 
